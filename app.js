9f4230b8e28a35830e2a890505b066036f3b409e
ahadmiaji <ahadmiaji@gmail.com>
2023-09-12 22:49:54 +0600

    updated

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const helmet = require("helmet");
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const hpp = require('hpp');
+const validator = require('validator');
 const router = require("./src/Routes/api");
 
-//base url
-//app.use('/api/v1',router);
+
+//middleware
+
 app.use(express.json());
 app.use(router);
+app.use(cors());
+app.use(helmet());
+app.use(
+    rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // Limit each IP to 100 requests per windowMs
+    })
+);
+app.use(mongoSanitize());
+app.use(hpp());
+
+//Routes
+
+// const apiRoutes = require("./src/Routes/api");
+// app.use("/api", apiRoutes);
+app.use('/api/v1', router);
+
 
 //Handle undefined routes(404)
 app.use((req, res, next) => {
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
-const PORT = process.env.RUNNING_PORT || 5400;
+const PORT = process.env.RUNNING_PORT || 5500;
 app.listen(PORT, () => {
     console.log(`Server is Running on ${PORT}`)
 });
